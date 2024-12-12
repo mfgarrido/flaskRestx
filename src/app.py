e1f54b643c568b51b99d507e13df372d984ca9f3
Matias <mati.figueroag@duocuc.cl>
2024-12-12 13:09:47 -0300

    se cambia el nombre al servicio

--- src/app.py
+++ src/app.py
@@ -7,11 +7,11 @@ from routes.pingpong import np_pingpong
 from services.juego_service import inicio
 from services.pingpong_service import pingpong
 
-app = Flask(__name__)
-api = Api(app)
+apirestx = Flask(__name__)
+api = Api(apirestx)
 
 api.add_namespace(juego_np,path='/juego')
 api.add_namespace(np_pingpong, path='/pingpong')
 
 if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    apirestx.run(debug=True, host='0.0.0.0', port=5000)
